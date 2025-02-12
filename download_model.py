403b7d7e5dd512fb57d7f1dd35dcf4259935ec9b
陈欢 <chenhuan@hudongtang.cn>
2025-02-12 21:33:01 +0800

    update

--- download_model.py
+++ download_model.py
@@ -1,5 +1,14 @@
 import os
 from faster_whisper import WhisperModel
+from huggingface_hub import hf_hub_download
+
+# 定义模型名称和存储路径
+model_name = "guillaumekln/faster-whisper-base.en"
+model_file = "model.bin"
+# 下载模型文件到本地
+local_model_path = hf_hub_download(repo_id=model_name, filename=model_file,local_dir="model")
+
+print(local_model_path)
 
 try:
     # 初始化 Whisper 模型，指定使用 GPU
@@ -8,3 +17,7 @@ try:
 except Exception as e:
     print(f"GPU 启用失败，将使用 CPU。错误信息: {e}")
     model = WhisperModel("base.en")
+
+
+
+    
